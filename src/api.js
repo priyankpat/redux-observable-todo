516aade9e25565279a693581645cbeed8224af48
Priyank Patel <priyank.patel@stackspace.ca>
2020-05-08 15:57:08 -0400

    Deploy to Firebase

--- src/api.js
+++ src/api.js
@@ -1,20 +1,19 @@
 import { pipe, map } from "rxjs/operators";
 import { ajax } from "rxjs/ajax";
 
-const BASE_URL =
-  "https://us-central1-redux-observable-todos.cloudfunctions.net/api";
+const BASE_URL = "https://us-central1-test-demo-cb0fd.cloudfunctions.net/api";
 
 export const getUser = () => ajax.getJSON(`${BASE_URL}/user`);
 export const getTodos = () => ajax.getJSON(`${BASE_URL}/todo`);
-export const addTodo = text =>
+export const addTodo = (text) =>
   ajax({
     url: `${BASE_URL}/todo`,
     method: "POST",
-    body: { text }
-  }).pipe(map(data => data.response));
+    body: { text },
+  }).pipe(map((data) => data.response));
 
-export const completeTodo = id =>
+export const completeTodo = (id) =>
   ajax({
     url: `${BASE_URL}/todo/${id}/complete`,
-    method: "POST"
-  }).pipe(map(data => data.response));
+    method: "POST",
+  }).pipe(map((data) => data.response));
